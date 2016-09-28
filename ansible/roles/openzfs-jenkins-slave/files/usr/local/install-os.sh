41c85b9804d140079c54107f31b52a78c5cad22a
Prakash Surya <prakash.surya@delphix.com>
2016-09-28 12:55:40 -0700

    Upgrade of "pkg:/package/pkg" is unnecessary

--- ansible/roles/openzfs-jenkins-slave/files/usr/local/install-os.sh
+++ ansible/roles/openzfs-jenkins-slave/files/usr/local/install-os.sh
@@ -11,9 +11,6 @@ source "${CI_SH_LIB}/common.sh"
 ONU="${PWD}/usr/src/tools/scripts/onu"
 REPO="${PWD}/packages/i386/nightly"
 
-log_must /usr/bin/pfexec /usr/bin/pkg \
-    install -g "${REPO}/repo.redist" pkg:/package/pkg
-
 log_must /usr/bin/pfexec "${ONU}" -t "openzfs-nightly" -d "${REPO}"
 
 exit 0
